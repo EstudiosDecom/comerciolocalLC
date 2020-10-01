817c3651000741bbbb1d293038368b3da028b9de
EstudiosDecom <17617549-4@MCONDES.CONDES.CL>
2020-10-01 12:21:26 -0300

    Comercios locales Programa Emprendimiento 2020

---
+++ data/Botillera_5.js
@@ -0,0 +1,11 @@
+var json_Botillera_5 = {
+"type": "FeatureCollection",
+"name": "Botillera_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "PLAZA DE LA CERVEZA SPA", "REPRESENTANTE LEGAL": "ALEX FRANCISCO SCHNAKE GALVEZ" }, "geometry": { "type": "Point", "coordinates": [ -70.57802, -33.41164 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ONE MARKET SPA", "REPRESENTANTE LEGAL": "JAIME GARMENDIA DE LA RIVERA" }, "geometry": { "type": "Point", "coordinates": [ -70.50869, -33.39944 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "AGRO MARKET S.A.", "REPRESENTANTE LEGAL": "IAN VASQUEZ TORO" }, "geometry": { "type": "Point", "coordinates": [ -70.52978, -33.41369 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "OBERST KADGIEN INVERSIONES LIMITADA", "REPRESENTANTE LEGAL": "LUCAS ROBERTO OBERST KADGIEN" }, "geometry": { "type": "Point", "coordinates": [ -70.54911, -33.39504 ] } }
+]
+}
---
+++ data/Pastelerayamasandera_7.js
@@ -0,0 +1,22 @@
+var json_Pastelerayamasandera_7 = {
+"type": "FeatureCollection",
+"name": "Pastelerayamasandera_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "NORMAN JAIMITO PAREDES TAPIA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.53564, -33.42376 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "COMERCIAL SUSANA CRUZ VASQUEZ E.I.R.L.", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.53519, -33.42399 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ALMACEN AL PASO SPA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.55317, -33.42558 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "JUAN CARLOS MARTINEZ FAUNDEZ", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.57538, -33.41162 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "OTONDO HERMANOS LIMITADA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.56565, -33.41705 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "INVERSIONES BETANCOURT LTDA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.56584, -33.4172 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "COMERCIALIZADORA Y DISTRIBUIDORA MARIA JOSE GONZALEZ E.I.R.L", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.57472, -33.4264 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "GASTRONOMICA SANTIAGO SANCHEZ HANISCH E.I.R.L.", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.55381, -33.39506 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "JORGE MAQUIVER VILLANUEVA VEGA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.53383, -33.4177 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "FRANCISCA PEASE POSADA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.59008, -33.42017 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ZUNINO CROCANTE LIMITADA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.55244, -33.41033 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "ROSSANA DEL CARMEN SOTO DEVIA", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.55103, -33.42593 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "DOLORES AGUSTINA VASQUEZ CONTRERAS", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.53738, -33.41394 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "COMERCIAL FRANCISCO JAVIER ESPINOZA E.I.R.L.", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.59525, -33.41744 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "JUAN FAUNDEZ CUEVAS", "GIRO": "PASTELERIA\/AMASANDERIA" }, "geometry": { "type": "Point", "coordinates": [ -70.53701, -33.42606 ] } }
+]
+}
